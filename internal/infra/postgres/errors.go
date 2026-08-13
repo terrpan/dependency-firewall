06f8c15a85d66b0555750733fe6f38c26c7aae9c
Daniel Terry <daniel.w.terry@gmail.com>
2026-08-13 22:50:33 +0200

    Add identity and hierarchy foundation

--- internal/infra/postgres/errors.go
+++ internal/infra/postgres/errors.go
@@ -21,3 +21,8 @@ func mapConstraintError(err error, constraints map[string]error) error {
 
 	return err
 }
+
+func isConstraint(err error, constraint string) bool {
+	var pgErr *pgconn.PgError
+	return errors.As(err, &pgErr) && pgErr.ConstraintName == constraint
+}
---
+++ migrations/000020_identity_and_hierarchy.down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS team_memberships;
+DROP TABLE IF EXISTS teams;
+DROP TABLE IF EXISTS organization_memberships;
+DROP TABLE IF EXISTS organizations;
+DROP TABLE IF EXISTS principal_identities;
+DROP TABLE IF EXISTS principals;
+DROP TABLE IF EXISTS tenant_identity_links;
